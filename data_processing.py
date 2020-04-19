701058ada2cd5a508809913a98121c129d95f46f
Abhisek Banerjee <banerjee.abhisek1990@gmail.com>
2020-04-19 05:14:00 -0500

    Adding dependency parsing features

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,8 @@ import os
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
+import spacy
+
 #Reference: https://stackoverflow.com/questions/25534214/nltk-wordnet-lemmatizer-shouldnt-it-lemmatize-all-inflections-of-a-word
 def is_noun(tag):
     return tag in ['NN', 'NNS', 'NNP', 'NNPS']
@@ -29,10 +31,11 @@ def penn_to_wn(tag):
     elif is_verb(tag):
         return wn.VERB
     return None
-
+nlp = spacy.load("en_core_web_sm")
 lemmatizer = WordNetLemmatizer()
 DIR = "WikipediaArticles"
 file_dict = dict()
+dependenc_parser_dict = dict()
 if __name__ == "__main__":
     for dirName, subdirList, fileList in os.walk(DIR):
         for file in fileList:
@@ -44,33 +47,43 @@ if __name__ == "__main__":
     for file, text in file_dict.items():
         lines = text.split("\n")
         token_list = list()
+        dep_list = list()
         for line in lines:
             try:
                 line = line.decode('utf-8')
-                tokens = nltk.word_tokenize(line)
-                tokens = nltk.pos_tag(tokens)
-                lemma_list = list()
-                for token in tokens:
-                    #print(token[0])
-                    if penn_to_wn(token[1]) == None:
-                        token_str = token[0]+":Lemmatized:"+lemmatizer.lemmatize(token[0])+":POS:"+token[1]
-                    else:
-                        token_str = token[0]+":Lemmatized:"+lemmatizer.lemmatize(token[0],penn_to_wn(token[1]))+":POS:"+token[1]
-                    synsets = wn.synsets(token[0])
-                    for synset in synsets:
-                        #print(str(synset)[8:-2])
-                        synset = str(synset)[8:-2]
-                        token_str += ":synset:"+str(synset)+":hyponyms:"+str(wn.synset(synset).hyponyms()) + ":hypernyms:"+str(wn.synset(synset).hypernyms()) + ":holonyms:"+str(wn.synset(synset).part_holonyms()) + ":meronyms:"+ str(wn.synset(synset).part_meronyms()) 
-                    lemma_list.append(token_str)
-                        
-
-                token_list.append(lemma_list)
+                sentences = nltk.sent_tokenize(line)
+                
+                for sentence in sentences:
+                    doc = nlp(sentence)
+                    tokens = nltk.word_tokenize(sentence)
+                    tokens = nltk.pos_tag(tokens)
+                    for word in doc:
+                        dep_list.append([word.text, word.dep_, word.shape_, word.is_alpha, word.is_stop, word.head.text, word.head.pos_, [child for child in word.children]])
+                    lemma_list = list()
+                    for token in tokens:
+                        #print(token[0])
+                        if penn_to_wn(token[1]) == None:
+                            token_str = token[0]+":Lemmatized:"+lemmatizer.lemmatize(token[0])+":POS:"+token[1]
+                        else:
+                            token_str = token[0]+":Lemmatized:"+lemmatizer.lemmatize(token[0],penn_to_wn(token[1]))+":POS:"+token[1]
+                        synsets = wn.synsets(token[0])
+                        for synset in synsets:
+                            #print(str(synset)[8:-2])
+                            synset = str(synset)[8:-2]
+                            token_str += ":synset:"+str(synset)+":hyponyms:"+str(wn.synset(synset).hyponyms()) + ":hypernyms:"+str(wn.synset(synset).hypernyms()) + ":holonyms:"+str(wn.synset(synset).part_holonyms()) + ":meronyms:"+ str(wn.synset(synset).part_meronyms()) 
+                        lemma_list.append(token_str)
+                    token_list.append(lemma_list)
             except Exception as e:
                 print(e)
         file_dict.update({file:token_list})
+        dependenc_parser_dict.update({file:dep_list}) 
 
 o_file = open("data_processing_output.txt", "w")
 for key, value in file_dict.items():
+    if key is not "":
+        o_file.write(str(key+": " + str(value)))
+        o_file.write("\n")
+for key, value in dependenc_parser_dict.items():
     if key is not "":
         o_file.write(str(key+": " + str(value)))
         o_file.write("\n")
